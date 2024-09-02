59a6951d0b794af4b65fd0d6ebc8b7325c52d885
anshika667 <139486225+anshika667@users.noreply.github.com>
2024-09-02 23:07:30 +0530

    Add files via upload

---
+++ about.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import '../css/about.css'
+function About() {
+  return (
+
+    <>
+        
+        <section id="about" class="about">
+            <div class="img-about">
+                <img src="images/ansh.jpg" alt=""/>
+                <div class="about-content order-last">
+                    <span>About Me</span>
+                    <p>Java Full Stack Developer</p>
+                    <h3 className='jumbotron'>I'm Anshika Bhargav & I'm from Delhi.<br/>I have completed an engineering diploma from<br/> GGPA specialized in computer science 
+                        with 80%.<br/> Along with my degree , I'm continuing java, SQL,<br/> and Full Stack courses online.
+                        Now I'm ready to apply my<br/> Knowledge into practice, though I do not have any real life<br/> experience.
+                        I've had a lot of exposure to the software field .<br/> My Goal is to build a successfull career as a Full <br/>stack developer.
+                        Moving for word in my careerI hope to <br/> expand my experience across different industries. <br/>I'm a good learner and team work 
+                        that's all about my self.
+                    </h3>
+                    <div class="action_btns">
+                    <button type="button" class="resume"><a href="ANSHIKA.pdf" download="anshika.pdf">Dowmload CV </a></button>
+                    </div>    
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+        {/* <div class="container ">
+           <div className="section_container section">
+           <div class="image">
+                 <img src="images\ansh.jpg" class="img-circle order-start" alt="" width="304" height="236"/> 
+              </div>
+              <div class="content ">
+              {/* <p class="subtitle">HELLO</p> */}
+                 {/* <h1>About Me</h1>
+                <h2 class="title">
+                   Front End Developer
+                </h2>
+                <p class="jumbotron order-last"> */}
+                {/* I'm Anshika Bhargav & I'm from Delhi.I have completed an engineering diploma from GGPA specialized in computer science 
+                        with 80%.Along with my degree , I'm continuing java, SQL, and Full Stack courses online.
+                        Now I'm ready to apply my Knowledge into practice, though I do not have any real life experience.
+                        I've had a lot of exposure to the software field . My Goal is to build a successfull career as a Full stack developer.
+                        Moving for word in my career I hope to expand my experience across different industries. I'm a good learner and team work 
+                        that's all about my self. */}
+                {/* </p>
+                </div>
+                </div> */}
+                {/* </div> 
+
+                    <div class="btn-box">
+                        <a href="#" class="btn">Read more</a> */}
+                    {/* </div>     */}
+           
+    
+    </>
+  )
+}
+
+export default About
